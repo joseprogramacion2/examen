46433eb83a63d98692c0323d96c16721cbfcf116
joseprogramacion2 <joseprogramacion2@gmail.com>
2022-08-09 19:49:33 -0600

    first commit

--- src/main/java/Examen/hola_mundo.java
+++ src/main/java/Examen/hola_mundo.java
@@ -3,7 +3,7 @@ package Examen;
 
 public class hola_mundo {
     public static void main(String[] args){
-    System.out.println("Bienvenido jose");
+    System.out.println("Jose Bienvenido");
     }
     
 }
